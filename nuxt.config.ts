44b8e6830030774570bed3ebb4ae01d809b7a032
Sergey Cherkashin <cherkashin@vuflya.com>
2024-10-23 03:01:41 +0300

    add linters & create main structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'src/',
+  css: ['~/assets/scss/index.scss'],
+
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
---
+++ src/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="layout">
+    <div class="main-container">
+      <h1 class="title">ikakprosto.ru</h1>
+
+      <slot />
+    </div>
+  </div>
+</template>
